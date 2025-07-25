9837e3516923ef181947fcfce7ebe1bc9caa5a6c
Anderson Juhasc <anderson.juhasc@gmail.com>
2025-07-25 20:57:07 -0300

    fix watch file

--- watch.sh
+++ watch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Watching for changes in .adoc files..."
-while inotifywait -e close_write -r . --include '\.adoc|.js$'; do
+while inotifywait -e close_write -r . --include '.*\.(adoc|js)$'; do
   echo "Change detected. Regenerating PDF..."
   asciidoctor-pdf -a pdf-theme=theme.yml index.adoc
 done
